711337d10eda015ce47d9b4967c4d607a845bc77
Raj <kmrrajj@gmail.com>
2018-05-17 15:55:42 +0530

    added code to handle unrelated actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -2,7 +2,9 @@ export default function({
   dispatch
 }) {
   return next => action => {
-    console.log(action);
-    next(action);
+    // If no promise, pass it to the next action.
+    if(!action.payload || !action.payload.then) {
+      return next(action);
+    }
   }
 }
